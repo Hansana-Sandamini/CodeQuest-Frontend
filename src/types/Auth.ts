2ad3aafc15dcc531a78a127c3fe0d37e4c561b36
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-11-29 19:28:55 +0530

    Add authentication request and response interfaces

---
+++ src/types/Auth.ts
@@ -0,0 +1,34 @@
+import type { IUser } from "./User"
+
+export interface LoginRequest {
+  email: string
+  password: string
+}
+
+export interface LoginResponse {
+  message: string
+  accessToken: string
+  refreshToken: string
+  user: IUser
+}
+
+export interface RegisterRequest {
+  firstname: string
+  lastname: string
+  username: string
+  email: string
+  password: string
+  profilePicture?: File | null
+}
+
+export interface RegisterResponse {
+  message: string
+  data: {
+    email: string
+    roles: string[]
+  }
+}
+
+export interface RefreshTokenResponse {
+  accessToken: string
+}
